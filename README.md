5c6777f9e2381605500be9cb21e295f273ee1c05
MAlnouri <alnourimahmmoud@gmail.com>
2020-02-25 23:41:10 -0800

    added basic card game + login functionality

--- README.md
+++
@@ -1,2 +0,0 @@
-# BlackJack
-Blackjack game
--- index.js
+++ index.js
@@ -1,56 +1,163 @@
-let userArray = [];
-
+let playerArray = [];
+let newOrOldUser = "None";
+let loginID;
 
 // define a constructor to create player objects
-var PlayerObject = function (pPlayerID, pName) {
-  this.PlayerID = pPlayerID;
-  this.PlayerName = pName;
+var Player = function (userName, password, wins, losses, money) {
+  this.userName = userName;
+  this.password = password;
+  this.wins = 0;
+  this.losses = 0;
+  this.money = 100;
+  this.ID = playerArray.length + 1;
 }
+
+playerArray.push(new Player("Mak", "abc123"));
+playerArray.push(new Player("Jack", "abc12345"));
+//console.log(playerArray[0].wins);
+playerArray[0].wins = 10;
+playerArray[0].losses = 2;
+playerArray[1].wins = 1;
+playerArray[1].losses = 5;
+playerArray[1].money = 500;
+//console.log(playerArray[0].wins);
 
 document.addEventListener("DOMContentLoaded", function () {
 
-player0 = new PlayerObject(0,"firstPerson");
-player1 = new PlayerObject(1,"secondPerson");
-player2 = new PlayerObject(2,"thirdPerson");
+  //pushes username and password to player array when login button is clicked
+  document.getElementById("buttonLogin").addEventListener("click", function () {
+    //changes value for existing or new user based on drop down choice selected
+    $(document).bind("change", "#select-user", function (event, ui) {
+      newOrOldUser = $('#select-user').val();
+    });
 
-userArray.push(player0);
-userArray.push(player1);
-userArray.push(player2);
+    if(newOrOldUser == "ExistingUser") {
+      //logs in as existing user
+      loginID = login(document.getElementById("user").value);
+      document.getElementById("loggedInAs").innerHTML = "Logged in as " + playerArray[loginID].userName;
+      //reveals logged in as element
+      document.getElementById("loggedInAs").style.display = "block";
 
-console.log(userArray);
+    } else if (newOrOldUser == "NewUser") {
+      //if new user, pushes user info to player array
+      playerArray.push(new Player(document.getElementById("user").value, document.getElementById("pass").value));
+      console.log(playerArray);
+    } else {
+      //if none selected, prompts user to select login type
+      console.log("Please select something");
+    }
+});
 
-$(document).on("pagebeforeshow", "#page2", function (event) {   // have to use jQuery 
- // document.getElementById("IDparmHere").innerHTML = "";
+  //sorts list by most money
+  document.getElementById("buttonSortMoney").addEventListener("click", function () {
+    playerArray.sort(dynamicSort("Money"));
+    createList();
+    document.location.href = "index.html#Hiscores";
+  });
+
+  //sorts list by most wins
+  document.getElementById("buttonSortWins").addEventListener("click", function () {
+    playerArray.sort(dynamicSort("Wins"));
+    createList();
+    document.location.href = "index.html#Hiscores";
+  });
+
+$(document).on("pagebeforeshow", "#Hiscores", function (event) {   // have to use jQuery 
+  //document.getElementById("IDparmHere").innerHTML = "";
   createList();
 });
+  
+  document.getElementById("buttonClear").addEventListener("click", function () {
+    document.getElementById("user").value = "";
+    document.getElementById("pass").value = "";
+    //document.getElementById("man").value = "";
+    //document.getElementById("woman").value = "";
+  });
+  
+$(document).on("pagebeforeshow", "#Login", function (event) {   // have to use jQuery 
+  document.getElementById("user").value = "";
+  document.getElementById("pass").value = "";
+  //document.getElementById("man").value = "";
+  //document.getElementById("woman").value = "";
+  });
+
+$(document).on("pagebeforeshow", "#page3", function (event) {   // have to use jQuery 
+  let localID =  document.getElementById("IDparmHere").innerHTML;
+  let w = playerArray[localID - 1].wins;
+  let l = playerArray[localID - 1].losses;
+  document.getElementById("listUser").innerHTML = "User Name: " + playerArray[localID-1].userName;
+  document.getElementById("listMoney").innerHTML = "Money $" + playerArray[localID - 1].money;
+  document.getElementById("listGames").innerHTML = "Games Played " + (w + l);
+  document.getElementById("listWins").innerHTML = "Wins " + playerArray[localID - 1].wins;
+  document.getElementById("listLoss").innerHTML = "Losses " + playerArray[localID - 1].losses;
+  document.getElementById("listWinrate").innerHTML = "Winrate " + (w / (w + l) * 100) + " %";
+ });
 
 });
 
 function createList()
 {
   // clear prior data
-  var divUserlist = document.getElementById("divUserlist");
-  while (divUserlist.firstChild) {    // remove any old data so don't get duplicates
-      divUserlist.removeChild(divUserlist.firstChild);
+  var divUserlist = document.getElementById("divMovieList");
+  while (divMovieList.firstChild) {    // remove any old data so don't get duplicates
+  divMovieList.removeChild(divMovieList.firstChild);
   };
 
   var ul = document.createElement('ul');  
-  userArray.forEach(function (element,) {   // use handy array forEach method
+  console.log(playerArray);
+  playerArray.forEach(function (element,) {   // use handy array forEach method
     var li = document.createElement('li');
-    li.innerHTML = "<a data-transition='pop' class='onePlayer' data-parm=" + element.PlayerID + "  href='#page3'>Jump </a> " + element.PlayerName;
+    li.innerHTML = "<a data-transition='pop' class='showUserName' data-parm=" + element.ID + "  href='#page3'>Get Details </a> " + element.ID + ":  " + element.userName + "  " + element.wins;
     ul.appendChild(li);
   });
-  divUserlist.appendChild(ul)
+  divMovieList.appendChild(ul)
 
-    // set up an event for each new li item, if user clicks any, it writes >>that<< items data-parm into the hidden html 
-    var classname = document.getElementsByClassName("onePlayer");
+    //set up an event for each new li item, if user clicks any, it writes >>that<< items data-parm into the hidden html 
+    var classname = document.getElementsByClassName("showUserName");
     Array.from(classname).forEach(function (element) {
         element.addEventListener('click', function(){
             var parm = this.getAttribute("data-parm");  // passing in the record.Id
-            console.log(parm);
             //do something here with parameter on  pickbet page
             document.getElementById("IDparmHere").innerHTML = parm;
             document.location.href = "index.html#page3";
         });
     });
-  } ;
+   
+};
+  
+
+/**
+ *  https://ourcodeworld.com/articles/read/764/how-to-sort-alphabetically-an-array-of-objects-by-key-in-javascript
+* Function to sort alphabetically an array of objects by some specific key.
+* 
+* @param {String} property Key of the object to sort.
+*/
+function dynamicSort(property) {
+  var sortOrder = 1;
+
+  if (property[0] === "-") {
+    sortOrder = -1;
+    property = property.substr(1);
+  }
+
+  return function (a, b) {
+    if (sortOrder == -1) {
+      return b[property].localeCompare(a[property]);
+    } else {
+      return a[property].localeCompare(b[property]);
+    }
+  }
+}
+
+//compares username and password to existing elements in array
+function login (userName, password) {
+  for (let i = 0; i < playerArray.length; i++) {
+    if(userName == playerArray[i].userName) {
+      console.log("Logging in...");
+      return i;
+    } else {
+      i++;
+    }
+  }
+  console.log("Login info was incorrect.")
+}
